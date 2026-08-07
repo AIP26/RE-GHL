9c43b2b78b4d54c66e269cf3a44055b8955ecd03
AIP26 <hutchinson060@hotmail.com>
2026-08-06 23:43:16 -0500

    Update render.js

--- src/lib/render.js
+++ src/lib/render.js
@@ -191,22 +191,6 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', '${esc(ga4)}');
 </script>` : ''}
-${pixelFacebook ? `
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '${esc(pixelFacebook)}');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=${esc(pixelFacebook)}&ev=PageView&noscript=1"
-/></noscript>` : ''}
 </head>
 <body>`;
 }
